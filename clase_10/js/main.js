f9ae1482e3bef834a3805dca9006312cc265a2d6
SebasRotondo <sebasrotondo@gmail.com>
2023-10-03 22:15:56 -0300

    clase 10 y otras modif

---
+++ clase_10/js/main.js
@@ -0,0 +1,78 @@
+/*//Clase 10 Intro JS//
+
+console.log ("Hola Mundo desde archivo externo");
+
+//variables
+
+var nom = "Harry";
+console.log (nom);
+
+// String    //nombre_apellido o nombreApellido
+let nombre = "Hermione";  //string
+const NOMBRE = "Harry";
+let edad = 20; //numerica
+
+const PI = 3.14;
+
+console.log (nombre);
+console.log (NOMBRE);
+
+let tenerMascotas = true; //boleana
+
+
+//operaciones con variables
+
+let numeroA = 12;
+let numeroB = 3;
+let resultadoSuma = numeroA + numeroB;
+console.log (resultadoSuma);
+
+let resultadoMul = numeroA * numeroB;
+console.log (resultadoMul);
+
+console.log (nombre +"\n" + apellido);
+*/
+/*
+let nombre = "Sebas";
+let apellido = "Rotondo";
+let edad = 50;
+let tenerMascotas = true;
+console.log (nombre);
+console.log (apellido);
+console.log (edad);
+console.log (tenerMascotas);
+
+
+let textoA = "Hola";
+let textoB = "Mundo";
+const BLANCO = " ";
+
+let resultadoA = textoA + BLANCO + textoB;
+console.log (resultadoA);*/
+
+// PROMPT
+/*
+let entrada = prompt ("Ingrese su nombre");
+let salida = "Hola " + entrada;
+console.log (salida);
+alert (salida);*/
+
+/*
+let numero = parseInt (prompt ("Ingrese un numero"));
+const NUMEROB = 30;
+let salida = numero + NUMEROB;
+alert (salida);*/
+
+let numero1 = parseInt (prompt("Ingrese el primer numero"));
+let numero2 = parseInt (prompt ("Ingrese el segundo numero"));
+
+let salida = numero1 + numero2;
+console.log (" La suma es: " + salida);
+alert ("La suma es: " + salida);
+
+
+
+
+
+
+
